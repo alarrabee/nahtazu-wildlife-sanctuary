6162af6c09f1ccdc4942b7cc824ab6014f0b0090
alarrabee <alexlarrabee10@gmail.com>
2024-08-17 09:32:54 -0500

    adjust text size on home page. center and resize carousel images. set images to autoplay

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Carousel } from 'antd';
 
-const contentStyle = {
-  margin: 0,
-  height: '900px',
-  width: '100%',
-  color: '#FFFFFF',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: 'white',
-  padding: "1%"
-};
-const imageStyle ={
-  width: '98%',
-}
+
+  
+  const imageContainerStyle = {
+    height: '400px', // Set a fixed height for the carousel
+    width: '100%',   // Ensure container takes full width
+    textAlign: 'center'
+  };
+  
+  const imageStyle = {
+    maxWidth: '70%',
+    maxHeight: '100%',
+    objectFit: 'cover', // Ensures the image covers the container
+    display: 'inline-block'
+  };
 
 const CarouselPic = () => {
   const onChange = (currentSlide) => {
@@ -21,20 +22,20 @@ const CarouselPic = () => {
   };
 
   return (
-    <Carousel afterChange={onChange}>
-        <div>
-            <h3 style={contentStyle}><img style ={imageStyle} src="https://images.unsplash.com/photo-1489015712802-f490a7a1061d?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Image of a girl infront of an Aquarium" /></h3>
-        </div>
-        <div>
-            <h3 style={contentStyle}><img style ={imageStyle} src="https://images.unsplash.com/photo-1706391160801-40cd74b22f38?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Image of Flamingos" /></h3>
-        </div>
-        <div>
-            <h3 style={contentStyle}><img style ={imageStyle} src="https://plus.unsplash.com/premium_photo-1683147718149-dac5d441db45?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Image of kids feeding a goat" /></h3>
-        </div>
-        <div>
-            <h3 style={contentStyle}><img style ={imageStyle} src="https://www.zoo-berlin.de/fileadmin/_processed_/f/a/csm_Afrikanischer_Loewe_-_Mateo_2__5a177d6871.jpg" alt="Image of Lion" /></h3>
-        </div>
-    </Carousel>
+        <Carousel afterChange={onChange} autoplay autoplaySpeed={4500}>
+            <div style ={imageContainerStyle}>
+                <h3 ><img  style ={imageStyle} src="https://www.zoo-berlin.de/fileadmin/_processed_/f/a/csm_Afrikanischer_Loewe_-_Mateo_2__5a177d6871.jpg" alt="Image of Lion" /></h3>
+            </div>
+            <div style ={imageContainerStyle}>
+                <h3 ><img style ={imageStyle} src="https://images.unsplash.com/photo-1489015712802-f490a7a1061d?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Image of a girl infront of an Aquarium" /></h3>
+            </div>
+            <div style ={imageContainerStyle}>
+                <h3 ><img style ={imageStyle} src="https://images.unsplash.com/photo-1706391160801-40cd74b22f38?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Image of Flamingos" /></h3>
+            </div>
+            <div style ={imageContainerStyle}>
+                <h3 ><img style ={imageStyle} src="https://plus.unsplash.com/premium_photo-1683147718149-dac5d441db45?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Image of kids feeding a goat" /></h3>
+            </div>
+        </Carousel>
   );
 };
 
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -10,36 +10,48 @@ import CarouselPic from '../components/Carousel'
 //     fontWeight: "bold",
 //     fontSize: "20px"
 //  };
-//  const mainTitles ={
-//     fontSize: "5.9vw"
 
-//  };
-//  const subTitles ={
-//     fontSize: "80px"
+ const mainTitle ={
+    fontSize: "2.8vw"
+ };
 
-//  };
-//  const descStyle ={
-//     fontWeight: "bold",
-//     fontSize: "30px"
+ const dividerStyle = {
+    borderColor: '#7cb305',
+  };
 
-//  };
+ const subTitles ={
+    fontSize: "2.5vw"
+
+ };
+
+ const paragraphStyle ={
+    fontWeight: "bold",
+    fontSize: "1.5vw"
+ };
 
 
 const Home = () => {
     return (
         <div>
-            <Divider style={{  borderColor: '#7cb305' }}><h1 className= "titles">Welcome to Nahtazu</h1></Divider>
-           <CarouselPic/>
+            <Divider style={{ ...dividerStyle, ...mainTitle }}>
+                <h1 className= "titles">Welcome to Nahtazu</h1>
+            </Divider>
+
+           <CarouselPic></CarouselPic>
            
-           <p className = 'normText' >At Nahtazu, our mission is to inspire and educate the public about wildlife conservation through immersive experiences and exceptional animal care. We are dedicated to preserving biodiversity and fostering a deeper understanding of the natural world, while promoting sustainable practices and engaging in global conservation efforts. Our commitment is to provide a safe, enriching environment for our animals and to create lasting connections between people and the planet.
+           <p className = 'normText' style={{ ...paragraphStyle }}>At Nahtazu, our mission is to inspire and educate the public about wildlife conservation through immersive experiences and exceptional animal care. We are dedicated to preserving biodiversity and fostering a deeper understanding of the natural world, while promoting sustainable practices and engaging in global conservation efforts. Our commitment is to provide a safe, enriching environment for our animals and to create lasting connections between people and the planet.
            </p>
-           
             
-           <Divider style={{  borderColor: '#7cb305' }}><h3 className= "titles">Checkout Our Amazing Animals Homes!</h3></Divider>
+           <Divider style={{  ...dividerStyle, ...subTitles }}>
+                <h3 className= "titles">Checkout Our Amazing Animals Homes!</h3>
+            </Divider>
 
            <AnimalList></AnimalList>
 
-           <Divider style={{  borderColor: '#7cb305' }}><h3 className= "titles">Why We think Zoos are Important</h3></Divider>
+           <Divider style={{  ...dividerStyle, ...subTitles }}>
+                <h3 className= "titles">Why We think Zoos are Important</h3>
+            </Divider>
+
             <p className = 'normText'  >Zoo conservation plays a critical role in preserving biodiversity and protecting endangered species in a world facing unprecedented environmental challenges. Modern zoos are far more than mere attractions; they are dynamic centers for conservation, education, and research. Through carefully managed breeding programs, zoos work to maintain genetic diversity and support the survival of species at risk of extinction. They also serve as vital research hubs, conducting studies on animal behavior, genetics, and health that contribute to broader conservation efforts. Zoos often collaborate with wildlife reserves and conservation organizations to address habitat loss, poaching, and climate change, extending their impact beyond their own facilities. Moreover, they educate the public about the importance of conservation, fostering a sense of responsibility and inspiring action to protect wildlife and their habitats. By combining these efforts, zoos help to create a more informed and engaged society, equipped to tackle the complex challenges facing our planet's diverse ecosystems.</p>
         </div>
     );
